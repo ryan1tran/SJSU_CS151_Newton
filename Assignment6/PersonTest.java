2eed5b21457c76a57b022645ab5803b88ad2e053
ryan1tran <tranryanp@gmail.com>
2020-03-06 12:41:43 -0800

    Completed Assignment6

---
+++ Assignment6/PersonTest.java
@@ -0,0 +1,122 @@
+package hw6;
+
+import java.util.*;
+
+public class PersonTest
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Person> list = new ArrayList<>();
+		
+		list.add(new Person("Joe", "Smith", 40));
+		list.add(new Person("Amy", "Gold", 32));
+		list.add(new Person("Tony", "Stork", 21));
+		list.add(new Person("Sean", "Irish", 24));
+		list.add(new Person("Tina", "Brock", 28));
+		list.add(new Person("Lenny", "Hep", 18));
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				if (p1.getAge() < p2.getAge())
+					return -1;
+				else if (p1.getAge() > p2.getAge())
+					return 1;
+				else
+					return 0;
+			}
+		});
+		
+		System.out.println("Sorted ascending by age:\n");
+		for (Person p : list)
+			System.out.println(p.getFirstName() + " " + p.getLastName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				if (p1.getAge() < p2.getAge())
+					return 1;
+				else if (p1.getAge() > p2.getAge())
+					return -1;
+				else
+					return 0;
+			}
+		});
+		
+		System.out.println("Sorted descending by age:\n");
+		for (Person p : list)
+			System.out.println(p.getFirstName() + " " + p.getLastName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				return p1.getFirstName().compareTo(p2.getFirstName());
+			}
+		});
+		
+		System.out.println("Sorted alphabetically by first name:\n");
+		for (Person p : list)
+			System.out.println(p.getFirstName() + " " + p.getLastName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				return p2.getFirstName().compareTo(p1.getFirstName());
+			}
+		});
+		
+		System.out.println("Sorted reverse-alphabetically by first name:\n");
+		for (Person p : list)
+			System.out.println(p.getFirstName() + " " + p.getLastName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				if (p1.getLastName().length() < p2.getLastName().length())
+					return -1;
+				else if (p1.getLastName().length() > p2.getLastName().length())
+					return 1;
+				else
+					return 0;
+			}
+		});
+		
+		System.out.println("Sorted ascending by last name length:\n");
+		for (Person p : list)
+			System.out.println(p.getLastName() + ", " + p.getFirstName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+		
+		Collections.sort(list, new Comparator<Person>()
+		{
+			public int compare(Person p1, Person p2)
+			{
+				if (p1.getLastName().length() < p2.getLastName().length())
+					return 1;
+				else if (p1.getLastName().length() > p2.getLastName().length())
+					return -1;
+				else
+					return 0;
+			}
+		});
+		
+		System.out.println("Sorted descending by last name length:\n");
+		for (Person p : list)
+			System.out.println(p.getLastName() + ", " + p.getFirstName() + ": " + p.getAge() + " years old.");
+		
+		System.out.println("---------------------------------\n");
+	}
+}
